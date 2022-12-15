9b0d4f0088eaf8306ce59981d8343f791c54ee61
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-12-15 08:07:19 -0500

    Challenge - View Model - Add button

--- from Udemy/Section 3_Working with View Models/Challenge 1/ViewModelDemo2/app/src/main/java/com/anushka/viewmodeldemo2/MainActivity.kt
+++ from Udemy/Section 3_Working with View Models/Challenge 1/ViewModelDemo2/app/src/main/java/com/anushka/viewmodeldemo2/MainActivity.kt
@@ -6,19 +6,35 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.anushka.viewmodeldemo2.databinding.ActivityMainBinding
 
+// Step 1 - do all steps to enable data binding
+// Step 2 - add required dependencies to use View model
+// Step 3 - synchronise them
+// Step 4 - create a new kotlin class MainActivityViewModel
 class MainActivity : AppCompatActivity() {
+    // Step 10 - Declare two object reference variable: for the data binding object and for the view model object
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Step 11 - create the ViewModel
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        // Step 12 - invoke getTotal() functions, using this viewModel and set to resultTextView text
         binding.resultTextView.text = viewModel.getTotal().toString()
 
+        // Step 13 set setOnClickListener on insertButton
         binding.insertButton.setOnClickListener {
-            viewModel.setTotal(binding.inputEditText.text.toString().toInt())
-            binding.resultTextView.text = viewModel.getTotal().toString()
 
+            // Step 14 - get value from edit text, convert to Int and invoke function setTotal(input: Int) using viewModel
+            viewModel.setTotal(binding.inputEditText.text.toString().toInt())
+
+            //Step 15 - set text to resultTextView by invoking function getTotal() using viewModel
+            binding.resultTextView.text = viewModel.getTotal().toString()
         }
     }
 }
+
+// This is how we use a view model to store and manage user interface controller data.
--- from Udemy/Section 3_Working with View Models/Challenge 1/ViewModelDemo2/app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModel.kt
+++ from Udemy/Section 3_Working with View Models/Challenge 1/ViewModelDemo2/app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModel.kt
@@ -2,14 +2,23 @@ package com.anushka.viewmodeldemo2
 
 import androidx.lifecycle.ViewModel
 
+// Step 5 - extend ViewModel
+// Step 6 - import the class (it might do it by default while you type)
+
 class MainActivityViewModel : ViewModel() {
+    // Step 7 - define a variable named total that holds integer value
     private var total = 0
 
-    fun getTotal():Int{
+    // Create two functions to get and set values of the variable
+
+    // Step 8 -  create a function getTotal() to return the total type Int
+    fun getTotal(): Int {
         return total
     }
 
-    fun setTotal(input:Int){
-        total +=input
+    // Step 9 -  create a function setTotal(input:Int) to return the total value of total + input after a button click.
+    fun setTotal(input: Int) {
+        // input value is added to the current total value
+        total += input
     }
 }
